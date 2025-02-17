4f55b512cee03ca911fc0b105f2ffa685fb26664
Hossein.M <hossein.m9877@gmail.com>
2025-02-17 15:33:24 +0330

    refactor: enhance documentation and improve code structure in DS1307 platform files

--- port/zynqPS-BSP/DS1307_platform.c
+++ port/zynqPS-BSP/DS1307_platform.c
@@ -1,45 +1,86 @@
-/*
- * DS1307_Platform.c
+/**
+ **********************************************************************************
+ * @file   DS1307_platform.c
+ * @author Hosseinali (https://github.com/iamhosseinali)
+ * @brief  DS1307 chip driver platform dependent part
+ *         Functionalities of the this file:
+ *          + Initialization the platform-dependent part of handler
+ **********************************************************************************
  *
- *  Created on: Jan 28, 2025
- *      Author: Hosseinali
+ * Copyright (c) 2021 Hossein.M (MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************************
  */
 
-
+/* Includes ---------------------------------------------------------------------*/
 #include "DS1307_platform.h"
 
 
-XIicPs Iic;		/**< Instance of the IIC Device */
-XIicPs_Config *Config;
+/* Private Variables ------------------------------------------------------------*/
+static IicPs Iic; /**< Instance of the IIC Device */
+static IicPs_Config *Config;
+
+
+
+/**
+ ==================================================================================
+                           ##### Private Functions #####                           
+ ==================================================================================
+ */
 
 static int8_t
-Platform_Init(void){
-	int Status;
-	/*
-	 * Initialize the IIC driver so that it's ready to use
-	 * Look up the configuration in the config table,
-	 * then initialize it.
-	 */
-	Config = XIicPs_LookupConfig(I2C_NUM_0);
-	if (NULL == Config) {
-		return -1;
-	}
-	Status = XIicPs_CfgInitialize(&Iic, Config, Config->BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return -1;
-	}
-	/*
-	 * Perform a self-test to ensure that the hardware was built correctly.
-	 */
-	Status = XIicPs_SelfTest(&Iic);
-	if (Status != XST_SUCCESS) {
-		return -1;
-	}
-	/*
-	 * Set the IIC serial clock rate.
-	 */
-	XIicPs_SetSClk(&Iic, IIC_SCLK_RATE);
+Platform_Init(void)
+{
+  int Status = 0;
+  /*
+   * Initialize the IIC driver so that it's ready to use
+   * Look up the configuration in the config table,
+   * then initialize it.
+   */
+  Config = XIicPs_LookupConfig(I2C_NUM_0);
+  if (NULL == Config)
+  {
+    return -1;
+  }
+  Status = XIicPs_CfgInitialize(&Iic, Config, Config->BaseAddress);
+  if (Status != XST_SUCCESS)
+  {
+    return -1;
+  }
+  /*
+   * Perform a self-test to ensure that the hardware was built correctly.
+   */
+  Status = XIicPs_SelfTest(&Iic);
+  if (Status != XST_SUCCESS)
+  {
+    return -1;
+  }
+  /*
+   * Set the IIC serial clock rate.
+   */
+  XIicPs_SetSClk(&Iic, IIC_SCLK_RATE);
+
+  return 0;
 }
+
 static int8_t
 Platform_DeInit(void)
 {
@@ -47,20 +88,24 @@ Platform_DeInit(void)
 }
 
 static int8_t
-Platform_WriteData(uint8_t Address, uint8_t *Data, uint8_t DataLen){
-    if (XIicPs_MasterSendPolled(&Iic, Data, DataLen, Address) != XST_SUCCESS)
-        return -1;
-	else
-		return 0;
+Platform_WriteData(uint8_t Address, uint8_t *Data, uint8_t DataLen)
+{
+  if (XIicPs_MasterSendPolled(&Iic, Data, DataLen, Address) != XST_SUCCESS)
+    return -1;
+
+  return 0;
 }
 
 static int8_t
-Platform_ReadData(uint8_t Address, uint8_t *Data, uint8_t DataLen){
-    if (XIicPs_MasterRecvPolled(&Iic, Data, DataLen, Address) != XST_SUCCESS)
-        return -1;
-	else
-		return 0;
+Platform_ReadData(uint8_t Address, uint8_t *Data, uint8_t DataLen)
+{
+  if (XIicPs_MasterRecvPolled(&Iic, Data, DataLen, Address) != XST_SUCCESS)
+    return -1;
+
+  return 0;
 }
+
+
 
 /**
  ==================================================================================
@@ -81,4 +126,3 @@ DS1307_Platform_Init(DS1307_Handler_t *Handler)
   Handler->PlatformSend = Platform_WriteData;
   Handler->PlatformReceive = Platform_ReadData;
 }
-
--- port/zynqPS-BSP/DS1307_platform.h
+++ port/zynqPS-BSP/DS1307_platform.h
@@ -1,18 +1,54 @@
-/*
- * DS1307_Platform.h
+/**
+ **********************************************************************************
+ * @file   DS1307_platform.h
+ * @author Hosseinali (https://github.com/iamhosseinali)
+ * @brief  DS1307 chip driver platform dependent part
+ *         Functionalities of the this file:
+ *          + Initialization the platform-dependent part of handler
+ **********************************************************************************
  *
- *  Created on: Jan 28, 2025
- *      Author: Hosseinali
+ * Copyright (c) 2021 Hossein.M (MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************************
  */
 
-#ifndef SRC_DS1307_PLATFORM_H_
-#define SRC_DS1307_PLATFORM_H_
+/* Define to prevent recursive inclusion ----------------------------------------*/
+#ifndef _DS1307_PLATFORM_H_
+#define _DS1307_PLATFORM_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Includes ---------------------------------------------------------------------*/
 #include "xiicps.h"
 #include "DS1307.h"
 
+
+/* Functionality Options --------------------------------------------------------*/
 #define IIC_SCLK_RATE		100000
-#define I2C_NUM_0			0
+#define I2C_NUM_0			  0
+
+
 
 /**
  ==================================================================================
@@ -28,4 +64,10 @@
 void
 DS1307_Platform_Init(DS1307_Handler_t *Handler);
 
-#endif /* SRC_DS1307_PLATFORM_H_ */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif //! _DS1307_PLATFORM_H_
