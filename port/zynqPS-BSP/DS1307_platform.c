c38cf0d4a088123cde75cb3719e661c8d46e77f8
Hossein.M <hossein.m9877@gmail.com>
2025-02-17 15:35:35 +0330

    chore: update copyright year in DS1307 platform files

--- port/zynqPS-BSP/DS1307_platform.c
+++ port/zynqPS-BSP/DS1307_platform.c
@@ -7,7 +7,7 @@
  *          + Initialization the platform-dependent part of handler
  **********************************************************************************
  *
- * Copyright (c) 2021 Hossein.M (MIT License)
+ * Copyright (c) 2025 Hossein.M (MIT License)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
